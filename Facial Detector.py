6c582ba0f07ae50924f5cd9166f6f531c24f1c08
AndreAraujoDev <andreearaujo98@gmail.com>
2019-07-17 16:23:57 -0300

    Saving detected faces in jpg format

--- Facial Detector.py
+++ Facial Detector.py
@@ -22,6 +22,11 @@ while True:
     #Desenha um retângulo em torno das faces detectadas
     for (x, y, w, h) in faces:
         cv2.rectangle(imagem, (x, y), (x+w, y+h), (255,0,0), 2)
+        #Pega o local onde o rosto foi detectado
+        roi_color = imagem[y:y + h, x:x + w]
+        #Se a tecla C for usada, captura o rosto e salva a imagem
+        if cv2.waitKey(1) & 0xFF == ord('c'):
+            cv2.imwrite(str(w) + str(h) + '_faces.jpg', roi_color)
 
     #Mostra a imagem capturada
     cv2.imshow('Video', imagem) 
@@ -29,7 +34,7 @@ while True:
     #Para o codigo e fecha a janela
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+       
 #Dispensa o uso da webcam
 webcam.release()
 #Fecha todas a janelas abertas
